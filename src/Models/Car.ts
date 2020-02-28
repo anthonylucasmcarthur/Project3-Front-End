1141c4db3cb63fca01d62f3b7dbe79df0014c8f2
Katie Hairston <kth5sf@virginia.edu>
2020-02-28 15:26:25 -0500

    Added Models for Entities

---
+++ src/Models/Car.ts
@@ -0,0 +1,20 @@
+import { Employee } from './Employee';
+
+export class Car {
+    car_id : number;
+    make : string;
+    model : string;
+    available_seats : string;
+    car_year : number; 
+    employee : Employee;
+
+    constructor(car_id : number, make : string, model : string, 
+                available_seats : string, car_year : number, employee : Employee) {
+                    this.car_id = car_id;
+                    this.make = make;
+                    this.model = model;
+                    this.available_seats = available_seats;
+                    this.car_year = car_year;
+                    this.employee = employee;
+                }
+}
