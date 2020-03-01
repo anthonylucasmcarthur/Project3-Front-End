3109654c154c3cee500ecfb3498072a5035f9879
waluigipr <waluigipr777@gmail.com>
2020-03-01 16:00:13 -0500

    Services fixed and updated

--- src/app/Services/config-service.service.ts
+++ src/app/Services/config-service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getConfigurationByLabel(label:string):Promise<string>{
+
+    return this.http.get<string>(`http://localhost:9999/configurations/${label}`).toPromise();
+  }
 }
