7b2b369a91fb02c1528f2f6c1a2eaf4abb46be32
Katie Hairston <kth5sf@virginia.edu>
2020-03-03 13:02:16 -0500

    changes

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+<<<<<<< Updated upstream
+=======
+import { Employee } from 'src/app/Models/Employee';
+import { EmployeeServiceService } from 'src/app/Services/employee-service.service';
+import { ConfigServiceService } from 'src/app/Services/config-service.service';
+import { Router } from '@angular/router';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-login',
@@ -14,8 +21,13 @@ export class LoginComponent implements OnInit {
   showManagerPage:boolean;
   showEmployeeProfile:boolean;
   showShowDrivers:boolean;
+<<<<<<< Updated upstream
 
   constructor() { }
+=======
+  
+  constructor(private es : EmployeeServiceService, private cs : ConfigServiceService, private r: Router) { }
+>>>>>>> Stashed changes
 
   ngOnInit() {
     this.showLogin = true;
@@ -26,9 +38,42 @@ export class LoginComponent implements OnInit {
   }
 
   submit(){
+<<<<<<< Updated upstream
 
     this.showLogin = false;
     this.showNavBar = true;
     // choose which page to show
   }
+=======
+    this.employeelogin();
+    //this.showLogin = false;
+   
+    // choose which page to show
+  }
+
+  async employeelogin(){
+    let username=((document.getElementById("username")as HTMLInputElement).value);
+    let password=((document.getElementById("password")as HTMLInputElement).value);
+    let empl:Employee = new Employee(0,"","","","",username,password,"",false,true,false,false,null);
+    let e:Employee = await this.es.login(empl);
+    if (e.is_manager) {
+      this.r.navigateByUrl("/manager");
+    }
+    else {
+      this.r.navigateByUrl("/profile");
+    }
+      console.log(e);
+      if(e != null){
+        let key = 'User'
+        this.cs.changeEmployee(e);
+        // sessionStorage.setItem(key,JSON.stringify(e));
+        // let user = JSON.parse(sessionStorage.getItem(key))
+        // console.log(user);
+      }
+      else {
+        console.log("c")
+        alert("Username or password doesn't exist!");
+      } 
+  }
+>>>>>>> Stashed changes
 }
--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -26,6 +26,7 @@ export class RegisterComponent implements OnInit {
     this.showLogin = false;
     this.showRegister = true;
   }
+<<<<<<< Updated upstream
 
   submit(){
     // validate address
@@ -33,6 +34,42 @@ export class RegisterComponent implements OnInit {
 
     this.showLogin = true;
     this.showRegister = false;
+=======
+   // this.employee_id = employee_id;
+  // this.email = email;
+  // this.first_name = first_name;
+  // this.last_name = last_name;
+  // this.phone_number = phone_number;
+  // this.username = username;
+  // this.password = password;
+  // this.user_address = user_address;
+  // this.is_accepting_rides = is_accepting_rides;
+  // this.is_active = is_active;
+  // this.isDriver = isDriver;
+  // this.is_manager = is_manager;
+  // this.office = office;
+  async submit(){
+  
+    let state=this.state;
+    let zip= this.zip;
+    let uaddress= this.street + ", "+ this.city + ", " + state + " " + zip;
+    let  empl:Employee = new Employee(0,this.email,this.fname,this.lname,this.phone,this.username,this.password,uaddress,true,true,this.checked,false,this.office);
+    let veri = await this.cs.verifyAddress(state ,this.city, this.street, zip);
+    console.log(veri);
+    let verstat = veri;
+    if(verstat) {
+      try {
+        let e:Employee =await this.es.addEmployee(empl);
+      } catch(e) {
+        console.log(e);
+      }
+    } else {
+      console.log("asdasd");
+    }
+    
+    // this.showLogin = true;
+    // this.showRegister = false;
+>>>>>>> Stashed changes
   }
 
 }
